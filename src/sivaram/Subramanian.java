5dd63b077413b71ecbaaa10e5d3bcd1e81b3d0a1
siva <194037@sivaramasubramanian.m@gmail.com>
2022-08-29 16:16:50 +0530

    3rd program:        spriting the digit

---
+++ src/sivaram/Subramanian.java
@@ -0,0 +1,16 @@
+package sivaram;
+
+public class Subramanian {
+public static void main(String[] args) {
+	int num=780; 
+	int a,b,c,d; 
+	a=num/100;
+	b=num%100; 
+	c=b/10; 
+	d=b%10;
+	System.out.println(a);
+	System.out.println(c);
+	System.out.println(d);
+	
+}
+}
