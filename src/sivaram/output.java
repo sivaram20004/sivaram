29965d2094f09df03267be6cacac814f82f1b482
siva <siva@siva>
2022-09-05 12:05:05 +0530

    find the output AB

--- src/sivaram/output.java
+++ src/sivaram/output.java
@@ -2,9 +2,22 @@ package sivaram;
 
 public class output {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			public static void main(String[] args) {
+				int a=3;
+				int b=5;
+				if(a>2 && a++>2)
+				{
+					++b;
+				}
+				else
+				{
+					a++;
+				}
+				System.out.println(a);
+				System.out.println(b);
+			}
 
+		
 	}
 
-}
+
