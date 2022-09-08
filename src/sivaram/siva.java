1e0f46e1b45f70091748a5dd6e3e14aa84c3c529
siva <siva@siva>
2022-09-08 18:35:26 +0530

    using for loop to multiply the number 1*2,2*3,4*5,5*6

---
+++ src/sivaram/siva.java
@@ -0,0 +1,18 @@
+package sivaram;
+
+public class siva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p=1;
+		for(int i=1;i<=6;i++)
+		{
+			p=i*(i+1);
+			
+			System.out.println(p);
+			
+		}
+
+	}
+
+}
