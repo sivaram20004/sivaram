560743b4e5a48aaa50cc4a83acd753ae514af097
siva <siva@siva>
2022-09-13 08:38:26 +0530

    jumping the value by using continue in jumping statement

---
+++ src/sivaram/jumping.java
@@ -0,0 +1,19 @@
+package sivaram;
+
+public class jumping {
+	public static void main (String args[]) {
+		int i=1;
+		while(i<7)
+		{
+			if(i==2)
+			{
+				i++;
+				continue;
+			}
+			System.out.println(i++);
+			}
+		}
+			
+	}
+
+
