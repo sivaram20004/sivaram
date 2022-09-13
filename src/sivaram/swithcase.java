fa1e358bb43b0a57c2ec9347e0ed22d594347eec
siva <siva@siva>
2022-09-13 08:59:22 +0530

    decision making:       another method switch case

---
+++ src/sivaram/swithcase.java
@@ -0,0 +1,22 @@
+package sivaram;
+
+public class swithcase { 
+	public static void main(String[] args) {
+		int n=5;
+		int r=n%2;
+		switch(r)
+		{
+			case 0:
+			System.out.println("even number");
+			break;
+			default:
+			System.out.println("odd number");
+		}
+			
+			
+			
+		}
+		
+	
+
+}
