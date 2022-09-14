579f76cd788ded9d57908a91f0db0cec054638c3
siva <siva@siva>
2022-09-14 11:58:35 +0530

    array initialization:         print the value in reverse by using array initialization in (FOR) loop

--- src/sivaram/arrayinitialization.java
+++ src/sivaram/arrayinitialization.java
@@ -5,13 +5,13 @@ public class arrayinitialization {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a[]= {1,2,3,4,5};
-		         
-		for(int i=0;i<a.length;i++)
-		{
-			System.out.println(a[i]);
-			
-		}
-
+		 for(int i=a.length-1;i>=0;i--) 
+		 {
+        	System.out.println(a[i]);
+        }
 	}
+        }
+	
 
-}
+
+
