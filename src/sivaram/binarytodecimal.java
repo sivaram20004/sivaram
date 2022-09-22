02b6973dce4ea5f620cd0260f0753b90048bda97
siva <siva@siva>
2022-09-22 14:43:55 +0530

    fabonic series

---
+++ src/sivaram/binarytodecimal.java
@@ -0,0 +1,18 @@
+package sivaram;
+
+public class binarytodecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int num=1010;
+         int a;
+         while(num>0)
+         {
+        	 a=num%10;
+        	 System.out.println(num);
+        	 num=num/10;
+        	
+         }
+	}
+
+}
