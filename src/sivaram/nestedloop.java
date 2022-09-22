1507a368e67daee1a902b0c5fccadffc2f0c5b1c
siva <siva@siva>
2022-09-22 14:14:04 +0530

    nested loop :          to print the ("*") like ***                                  ***                                  ***

---
+++ src/sivaram/nestedloop.java
@@ -0,0 +1,17 @@
+package sivaram;
+
+public class nestedloop {
+	public static void main(String[] args) {
+		int n=3;
+		for (int j=0;j<3;j++)
+		{
+			for(int i=0;i<n;i++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
