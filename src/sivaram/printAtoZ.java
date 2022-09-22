e7f8e562adb619be658e42680752c2fb401fe211
siva <siva@siva>
2022-09-22 15:04:11 +0530

    print A to Z

---
+++ src/sivaram/printAtoZ.java
@@ -0,0 +1,12 @@
+package sivaram;
+
+public class printAtoZ {
+	 public static void main(String[] args) { 
+		 char c;
+		 for(c='a';c<='z';c++)
+		 {			 
+			 System.out.println(c);
+			 
+	 }
+}
+}
