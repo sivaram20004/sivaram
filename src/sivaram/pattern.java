048f370cb54515cbf88a286d586d670f040e5de6
siva <siva@siva>
2022-09-22 15:24:05 +0530

    pattern program :          to print 1 2 3 4 5 67 8 9 10

---
+++ src/sivaram/pattern.java
@@ -0,0 +1,23 @@
+package sivaram;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=3;  
+		int p=1;
+		for(int i=0;i<n;i++) 
+		{ 
+			for(int j=0;j<n;j++)
+		{
+				
+			
+        		  System.out.print(p++);
+        	  
+			} 
+        	  }		
+		 System.out.println();	
+		}
+	}
+
+
